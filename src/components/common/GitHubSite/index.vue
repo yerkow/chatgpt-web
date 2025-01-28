2973b1e73ff9f56ad3d2d7bd4c84ddf16437f36b
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-01-29 00:55:46 +0500

    fix: clear

--- src/components/common/GitHubSite/index.vue
+++ src/components/common/GitHubSite/index.vue
@@ -1,17 +1,12 @@
-<script setup lang="ts">
-const releaseVersion = import.meta.env.VITE_RELEASE_VERSION || 'v0.0.0'
-</script>
-
 <template>
-	<div class="text-center text-xs mb-5">
+  <div class="text-center text-xs mb-5">
     <span class="text-neutral-400">
       Powered by
     </span>
-		<a href="https://foxminded.space" target="_blank">
+    <a href="https://foxminded.space" target="_blank">
       <span class="text-blue-500">
         iSpark
       </span>
-		</a>
-		<span v-text="`${releaseVersion}`"/>
-	</div>
+    </a>
+  </div>
 </template>
